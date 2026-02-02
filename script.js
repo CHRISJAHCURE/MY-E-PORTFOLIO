d298e9522e768ebe0e9dae7ac74fe5c2b7366518
CHRISJAHCURE <omollochrisryan24@students.tukenya.ac.ke>
2026-02-02 14:24:51 +0300

    UPLOAD E-PORTFOLIO FILES

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("contactform")
+.addEventListener("submit",  function(event) {
+  event.preventDefault();
+  alert("MESSAGE SENT SUCCESSFULYY");
+});
+
+alert("JAVASRIPT CONNECTED SUCCESSFULLY!");
